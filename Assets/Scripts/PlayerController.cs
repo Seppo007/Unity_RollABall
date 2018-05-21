f84634c243c214996febdd1996f18ad83306429c
Sebastian Schalow <schalowseb@gmail.com>
2018-05-21 16:23:47 +0200

    Add UI text fields

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     // Use this for initialization
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = string.Empty;
     }
 
     // Update is called once per frame
@@ -40,6 +46,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 
